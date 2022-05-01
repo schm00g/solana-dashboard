6d1f176b184c4b5987c493cc5cb8ecb2aeb14c3b
schm00g <hk8igureu@relay.firefox.com>
2022-05-01 16:27:23 +0100

    set solana balance

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,6 +1,27 @@
+import { useMoralisSolanaApi, setSolanaBalance } from "react-moralis";
+import { useEffect, useState } from 'react';
+
 export default function Dashboard({logout, user}) {
 
     let walletAddress = user.get('solAddress');
+    let SolanaAPI = useMoralisSolanaApi();
+
+    let [solanaBalance, setSolanaBalance] = useState('');
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let result = await SolanaAPI.account.balance({
+                    network: 'devnet',
+                    address: walletAddress
+                });
+                setSolanaBalance(result.solana);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchData();
+    }, []);
 
     return(
         <div className="w-screen h-screen flex flex-col items-center justify-center py-10 px-4 bg-black overflow-auto">
@@ -10,7 +31,7 @@ export default function Dashboard({logout, user}) {
             <div className="w-full h-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
                 <div className="bg-pink rounded-2xl drop-shadow-md px-2 py-2 md:px-4 md:py-4 md:text-lg">
                     <p className="text-2xl md:text-4xl">solana balance</p>
-                    <p className="mt-4 md:mt-10 text-3xl md:text-6xl">1.2356 <span>SOL</span></p>
+                    <p className="mt-4 md:mt-10 text-3xl md:text-6xl">{solanaBalance}<span>SOL</span></p>
                 </div>
                 <div className="bg-green rounded-2xl drop-shadow-md px-2 py-2 md:px-4 md:py-4 md:text-lg">
                     <p className="text-2xl md:text-4xl">spl tokens 2</p>
