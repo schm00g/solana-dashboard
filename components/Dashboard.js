d04bb47c0afbf1798daa8d2b1e14bcff524fae64
schm00g <hk8igureu@relay.firefox.com>
2022-05-01 19:38:18 +0100

    conditional rendering based on loading state

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -9,6 +9,7 @@ export default function Dashboard({logout, user}) {
     let [solanaBalance, setSolanaBalance] = useState('');
     let [splBalance, setSplBalance] = useState([{}]);
     let [nftsBalance, setNftsBalance] = useState([{}]);
+    let [isLoading, setIsLoading] = useState(true);
     
     
     useEffect(() => {
@@ -42,6 +43,7 @@ export default function Dashboard({logout, user}) {
             } catch (error) {
                 console.log(error);
             }
+            setIsLoading(false);
         }
         fetchData();
     }, []);
@@ -54,20 +56,20 @@ export default function Dashboard({logout, user}) {
             <div className="w-full h-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
                 <div className="bg-pink rounded-2xl drop-shadow-md px-2 py-2 md:px-4 md:py-4 md:text-lg">
                     <p className="text-2xl md:text-4xl">solana balance</p>
-                    <p className="mt-4 md:mt-10 text-3xl md:text-6xl">{solanaBalance.slice(0, 6)}<span> SOL</span></p>
+                    {!isLoading && <p className="mt-4 md:mt-10 text-3xl md:text-6xl">{solanaBalance.slice(0, 6)}<span> SOL</span></p>} 
                 </div>
                 <div className="bg-green rounded-2xl drop-shadow-md px-2 py-2 md:px-4 md:py-4 md:text-lg">
-                    <p className="text-2xl md:text-4xl">spl tokens {splBalance.length}</p>
+                    <p className="text-2xl md:text-4xl">spl tokens {!isLoading ? splBalance.length : ''}</p>
                     <ul className="list-disc ml-8 mt-4 md:mt-10 text-md md:text-lg">
-                        {splBalance.length > 0 && splBalance.map((spl, index) => (
+                        {splBalance.length > 0 && !isLoading && splBalance.map((spl, index) => (
                             <li key={index}>{spl.mint?.slice(0, 12)}...{''}{spl.amount}</li>
                         ))}
                     </ul>
                 </div>
                 <div className="bg-cyan md:col-span-2 rounded-2xl drop-shadow-md px-2 py-2 md:px-4 md:py-4 md:text-lg">
-                    <p className="text-2xl md:text-4xl">nfts {nftsBalance.length}</p>
+                    <p className="text-2xl md:text-4xl">nfts {!isLoading && nftsBalance.length}</p>
                     <ul className="list-disc px-4 mt-4 md:mt-10 text-md md:text-lg">
-                        {nftsBalance.length > 0 && nftsBalance.map((nft, index) => (
+                        {nftsBalance.length > 0 && !isLoading && nftsBalance.map((nft, index) => (
                             <li className="text-ellipsis overflow-hidden" key={index}>{nft.mint}</li>
                         ))}
                     </ul>
